7b8c72d2a13dff4f4f85fe5b3ab2e935810767be
raselkibria <mdraselkibria11@gmail.com>
2022-06-06 18:55:37 +0600

    some change

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Shop = () => {
     const [data,setData]=useState([])
