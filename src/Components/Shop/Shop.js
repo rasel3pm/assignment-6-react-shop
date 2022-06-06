99ed605250d27ee8956d769ca6a7c4ce44451efe
raselkibria <mdraselkibria11@gmail.com>
2022-06-06 18:27:12 +0600

    first commit

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const Shop = () => {
+    const [data,setData]=useState([])
+    const [search,setSearch]=useState("")
+
+
+    useEffect(()=>{
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then((res) => res.json())
+        .then((data) => {
+            setData(data.meals);
+            console.log(data.meals)
+        })
+    },[search])
+
+    const searchFood=(e)=>{
+        setSearch(e.target.value)
+    }
+
+
+    return (<div className='container'>
+        <input type="text" onChange={searchFood} placeholder='Food Search'/>
+        <div className='abc'>
+            {
+                data.map(item=> 
+                    <div className='product' key={item.idMeal}>
+                        <div className="item">
+                            <a href="#">
+                            <img src={item.strMealThumb} alt="" />
+                            <p>{item.strMeal}</p>
+                            </a>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+        <footer>
+            <div className="footer-contant">
+                <p>All right served by Md Rasel Kibria</p>
+            </div>
+        </footer>
+        </div>
+    );
+};
+
+export default Shop;
